be168bb9a94aead93727a02a4f93d333d4effe06
Fatma Ali <alifatma2019@gmail.com>
2019-10-17 09:30:33 +0300

    JJ's feedback

--- src/summary/patient-chart-summary.component.tsx
+++ src/summary/patient-chart-summary.component.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { RouteComponentProps } from "react-router";
+import { getCurrentPatient } from "@openmrs/esm-api";
 import styles from "./patient-chart-summary.css";
 import ProfileSection from "./profile/profile-section.component";
 
 export default function PatientChartSummary(props: PatientChartSummaryProps) {
+  const [currentPatient, setCurrentPatient] = React.useState(null);
+
+  React.useEffect(() => {
+    const subscription = getCurrentPatient().subscribe(patient => {
+      setCurrentPatient(patient);
+    });
+
+    return () => subscription.unsubscribe();
+  });
+
   return (
     <main className={`omrs-main-content ${styles.patientSummary}`}>
-      <ProfileSection match={props.match} />
+      <ProfileSection match={props.match} patient={currentPatient} />
     </main>
   );
 }
